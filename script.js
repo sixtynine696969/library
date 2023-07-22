da7ee78effa8708599f3b814f14ef750865211e1
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-07-22 16:46:35 +0200

    Some bit of JS idk

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+    this.info = () => `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read'}`
+}
+
+function addBookToLibrary() {
+    
+}
